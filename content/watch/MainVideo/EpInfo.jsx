e46f4584fa08b0d1f1f43f1bf166fc288699eb13
AE <antonioeid40@gmail.com>
2025-02-04 18:33:02 +0200

    fix: correct typo in server message in EpInfo component

--- content/watch/MainVideo/EpInfo.jsx
+++ content/watch/MainVideo/EpInfo.jsx
@@ -5,7 +5,7 @@ const EpInfo = ({ episode }) => {
         You are watching <span className="text-[#8B0000] font-semibold">Episode {episode}</span>
       </p>
       <p className="text-[#ffffff6a] text-xs italic">
-        If this server won't work, switch to another
+        If this server wont work, switch to another
       </p>
       <div className="absolute bottom-2 left-0 right-0 h-[2px] bg-gradient-to-r from-transparent via-[#8B0000]/50 to-transparent opacity-50"></div>
     </div>
